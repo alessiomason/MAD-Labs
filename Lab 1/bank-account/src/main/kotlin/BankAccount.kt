889c4964139aa28e615f8fd8d88a2b5828644b72
Alessio Mason <alessio.mason@me.com>
2023-03-08 11:59:05 +0100

    Lab 1 bank-account: AtomicBoolean

--- Lab 1/bank-account/src/main/kotlin/BankAccount.kt
+++ Lab 1/bank-account/src/main/kotlin/BankAccount.kt
@@ -1,23 +1,23 @@
+import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicLong
 
 class BankAccount {
     private var atomicBalance = AtomicLong(0)
     val balance: Long
         get() {
-            if (closed) throw IllegalStateException("The account is closed!")
+            if (closed.get()) throw IllegalStateException("The account is closed!")
             else return atomicBalance.get()
         }
 
-    private var closed = false
+    private var closed = AtomicBoolean(false)
 
     fun adjustBalance(amount: Long) {
-        if (closed)
-            throw IllegalStateException("The account is closed!")
+        if (closed.get()) throw IllegalStateException("The account is closed!")
 
         atomicBalance.addAndGet(amount)
     }
 
     fun close() {
-        closed = true
+        closed.set(true)
     }
 }
