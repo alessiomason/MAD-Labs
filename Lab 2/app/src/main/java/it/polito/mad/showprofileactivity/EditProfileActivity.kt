8a2d6cb538fac69add7bd5cebf2824a0611e249b
s303447 <s303447@studenti.polito.it>
2023-03-28 11:54:07 +0200

    switch tra edit profile e show profile

--- Lab 2/app/src/main/java/it/polito/mad/showprofileactivity/EditProfileActivity.kt
+++ Lab 2/app/src/main/java/it/polito/mad/showprofileactivity/EditProfileActivity.kt
@@ -1,30 +1,27 @@
 package it.polito.mad.showprofileactivity
 
-
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
 
-
-class MainActivity : AppCompatActivity() {
+class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_edit_profile)
     }
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu to use in the action bar
         val inflater = menuInflater
-        inflater.inflate(R.menu.menu, menu)
+        inflater.inflate(R.menu.menu_edit_profile, menu)
         return super.onCreateOptionsMenu(menu)
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle presses on the action bar menu items
         when (item.itemId) {
-            R.id.modify_profile -> {
-                val intent = Intent(this, EditProfileActivity::class.java)
+            R.id.save_profile -> {
+                val intent = Intent(this, MainActivity::class.java)
                 // start your next activity
                 startActivity(intent)
                 return true
@@ -32,6 +29,4 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
-
-
 }
