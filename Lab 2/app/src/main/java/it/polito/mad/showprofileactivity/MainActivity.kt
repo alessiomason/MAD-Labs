a469c2ad2bff00fdc7f9f539b5daf3b71e689d69
s303447 <s303447@studenti.polito.it>
2023-03-28 09:26:49 +0200

    added svg files

--- Lab 2/app/src/main/java/it/polito/mad/showprofileactivity/MainActivity.kt
+++ Lab 2/app/src/main/java/it/polito/mad/showprofileactivity/MainActivity.kt
@@ -1,11 +1,20 @@
 package it.polito.mad.showprofileactivity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
