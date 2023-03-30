356024551ba1839fa5cdca4bce730c7047d31c78
s303447 <s303447@studenti.polito.it>
2023-03-30 10:35:12 +0200

    lentissimi passi in avanti

--- Lab 2/app/src/main/java/it/polito/mad/showprofileactivity/EditProfileActivity.kt
+++ Lab 2/app/src/main/java/it/polito/mad/showprofileactivity/EditProfileActivity.kt
@@ -3,13 +3,55 @@ package it.polito.mad.showprofileactivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Nickname
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
 
 class EditProfileActivity : AppCompatActivity() {
+    lateinit var name:EditText
+    lateinit var nickname:EditText
+    lateinit var age:EditText
+    lateinit var bio:EditText
+    lateinit var phone:EditText
+    lateinit var location:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+        name=findViewById(R.id.editTextFullName)
+        nickname=findViewById(R.id.editTextNickname)
+        age=findViewById(R.id.editTextAge)
+        bio=findViewById(R.id.editTextBio)
+        phone=findViewById(R.id.editTextPhone)
+        location=findViewById(R.id.editTextLocation)
+
+        if(savedInstanceState!=null) {
+            name.setText(savedInstanceState.getString("chiave" ))
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState?.putString("name",name.text.toString())
+        outState?.putString("nickname",nickname.text.toString())
+        outState?.putString("bio",bio.text.toString())
+        outState?.putInt("age",age.text.toString().toInt())
+        outState?.putString("phone",phone.text.toString())
+        outState?.putString("location",location.text.toString())
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        name.setText(savedInstanceState.getString("name"));
+        nickname.setText(savedInstanceState.getString("nickname"));
+        bio.setText(savedInstanceState.getString("bio"));
+        age.setText(savedInstanceState.getString("age"));
+        phone.setText(savedInstanceState.getString("phone"));
+        location.setText(savedInstanceState.getString("location"));
+
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu to use in the action bar
@@ -22,6 +64,15 @@ class EditProfileActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.save_profile -> {
                 val intent = Intent(this, MainActivity::class.java)
+
+                val outState = Bundle();
+                outState?.putString("name",name.text.toString())
+                outState?.putString("nickname",nickname.text.toString())
+                outState?.putString("bio",bio.text.toString())
+                outState?.putString("age",age.text.toString())
+                outState?.putString("phone",phone.text.toString())
+                outState?.putString("location",location.text.toString())
+                intent.putExtras(outState);
                 // start your next activity
                 startActivity(intent)
                 return true
--- Lab 2/app/src/main/java/it/polito/mad/showprofileactivity/MainActivity.kt
+++ Lab 2/app/src/main/java/it/polito/mad/showprofileactivity/MainActivity.kt
@@ -5,13 +5,38 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var name: TextView
+    lateinit var nickname: TextView
+    lateinit var age: TextView
+    lateinit var bio: TextView
+    lateinit var phone: TextView
+    lateinit var location: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        name=findViewById(R.id.textFullName)
+        nickname=findViewById(R.id.textNickname)
+        age=findViewById(R.id.textAge)
+        bio=findViewById(R.id.textBio)
+        phone=findViewById(R.id.textPhone)
+        location=findViewById(R.id.textLocation)
+
+        if(savedInstanceState!=null)
+        {
+            name.setText(savedInstanceState.getString("name"))
+            nickname.setText(savedInstanceState.getString("nickname"))
+            age.setText(savedInstanceState.getString("age"))
+            bio.setText(savedInstanceState.getString("bio"))
+            phone.setText(savedInstanceState.getString("phone"))
+            location.setText(savedInstanceState.getString("location"))
+
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -26,6 +51,14 @@ class MainActivity : AppCompatActivity() {
             R.id.modify_profile -> {
                 val intent = Intent(this, EditProfileActivity::class.java)
                 // start your next activity
+                val outState = Bundle();
+                outState?.putString("name",name.text.toString())
+                outState?.putString("nickname",nickname.text.toString())
+                outState?.putString("bio",bio.text.toString())
+                outState?.putString("age",age.text.toString())
+                outState?.putString("phone",phone.text.toString())
+                outState?.putString("location",location.text.toString())
+                intent.putExtras(outState);
                 startActivity(intent)
                 return true
             }
