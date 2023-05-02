3a2b46e41b536854f387fde5d32036966303b240
Alessio Mason <alessio.mason@me.com>
2023-05-02 13:16:59 +0200

    Lab 3: week calendar with titles

--- Lab 3/app/src/main/java/it/polito/mad/playgroundsreservations/reservations/PlaygroundsAvailabilityFragment.kt
+++ Lab 3/app/src/main/java/it/polito/mad/playgroundsreservations/reservations/PlaygroundsAvailabilityFragment.kt
@@ -1,19 +1,17 @@
 package it.polito.mad.playgroundsreservations.reservations
 
-import android.content.res.Resources.Theme
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.view.children
 import androidx.fragment.app.Fragment
-import com.kizitonwose.calendar.core.CalendarMonth
+import com.kizitonwose.calendar.core.Week
 import com.kizitonwose.calendar.core.WeekDay
 import com.kizitonwose.calendar.core.atStartOfMonth
 import com.kizitonwose.calendar.core.daysOfWeek
-import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import com.kizitonwose.calendar.core.yearMonth
 import com.kizitonwose.calendar.view.ViewContainer
 import com.kizitonwose.calendar.view.WeekCalendarView
 import com.kizitonwose.calendar.view.WeekDayBinder
@@ -21,19 +19,34 @@ import com.kizitonwose.calendar.view.WeekHeaderFooterBinder
 import it.polito.mad.playgroundsreservations.R
 import java.time.LocalDate
 import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 import java.time.format.TextStyle
 import java.util.Locale
 
 class PlaygroundsAvailabilityFragment: Fragment(R.layout.fragment_playgrounds_availability) {
 
     private lateinit var weekCalendarView: WeekCalendarView
-    private var selectedDate: LocalDate? = null
+    private var selectedDate = LocalDate.now()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         weekCalendarView = view.findViewById(R.id.week_calendar_view)
 
+        val currentDate = LocalDate.now()
+        val currentMonth = YearMonth.now()
+        val startDate = currentMonth.minusMonths(12).atStartOfMonth()
+        val endDate = currentMonth.plusMonths(12).atEndOfMonth()
+        val daysOfWeek = daysOfWeek()
+
+        // change month title
+        weekCalendarView.weekScrollListener = { newWeek ->
+            val monthHeaderTextView = view.findViewById<TextView>(R.id.month_header_text)
+            val formatter = DateTimeFormatter.ofPattern("MMM uuuu", Locale.getDefault())
+            monthHeaderTextView.text = newWeek.days[0].date.yearMonth.format(formatter)
+        }
+
+        // handles the newly selected date
         weekCalendarView.dayBinder = object: WeekDayBinder<DayViewContainer> {
             // Called only when a new container is needed.
             override fun create(view: View) = DayViewContainer(view)
@@ -54,12 +67,33 @@ class PlaygroundsAvailabilityFragment: Fragment(R.layout.fragment_playgrounds_av
             }
         }
 
-        val currentDate = LocalDate.now()
-        val currentMonth = YearMonth.now()
-        val startDate = currentMonth.minusMonths(12).atStartOfMonth() // Adjust as needed
-        val endDate = currentMonth.plusMonths(12).atEndOfMonth()  // Adjust as needed
-        val firstDayOfWeek = firstDayOfWeekFromLocale() // Available from the library
-        weekCalendarView.setup(startDate, endDate, firstDayOfWeek)
+        // shows the days names
+        weekCalendarView.weekHeaderBinder = object: WeekHeaderFooterBinder<WeekViewContainer> {
+            override fun create(view: View) = WeekViewContainer(view)
+            override fun bind(container: WeekViewContainer, data: Week) {
+
+                // Remember that the header is reused so this will be called for each month.
+                // However, the first day of the week will not change so no need to bind
+                // the same view every time it is reused.
+                if (container.titlesContainer.tag == null) {
+                    container.titlesContainer.tag = data.days
+                    container.titlesContainer.children.map { it as TextView }
+                        .forEachIndexed { index, textView ->
+                            val dayOfWeek = daysOfWeek[index]
+                            val title = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+                            textView.text = title
+                            // In the code above, we use the same `daysOfWeek` list
+                            // that was created when we set up the calendar.
+                            // However, we can also get the `daysOfWeek` list from the month data:
+                            // val daysOfWeek = data.weekDays.first().map { it.date.dayOfWeek }
+                            // Alternatively, you can get the value for this specific index:
+                            // val dayOfWeek = data.weekDays.first()[index].date.dayOfWeek
+                        }
+                }
+            }
+        }
+
+        weekCalendarView.setup(startDate, endDate, daysOfWeek.first())
         weekCalendarView.scrollToWeek(currentDate)
     }
 
@@ -81,5 +115,11 @@ class PlaygroundsAvailabilityFragment: Fragment(R.layout.fragment_playgrounds_av
                 t.text = day.toString()
             }
         }
+    }
+
+    class WeekViewContainer(view: View) : ViewContainer(view) {
+        // Alternatively, you can add an ID to the container layout and use findViewById()
+        val monthTitleContainer = view as ViewGroup
+        val titlesContainer = view as ViewGroup
     }
 }
